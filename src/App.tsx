c6968d8a9d58f2bb5f4fdd135d0469214514b5b3
Ken Tran <khanht@udel.edu>
2025-08-28 18:26:18 -0700

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. This is Ken Tran (702616829)
             </p>
         </div>
     );
