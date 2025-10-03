c65fc6569f8a77eef57c74ad0231df1f5b53f5de
Ken Tran <khanht@udel.edu>
2025-10-03 01:02:41 -0400

    'Finished_form'

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    const updateAnswer = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setAnswer(event.target.value);
+    };
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group>
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            <div>Solution Found: {answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selected, setSelected] = useState<string>(options[0]);
+    const updateSelected = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelected(event.target.value);
+    };
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Label>Select your answer below:</Form.Label>
+                <Form.Select value={selected} onChange={updateSelected}>
+                    {options.map((answer: string) => (
+                        <option key={answer} value={answer}>
+                            {answer}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>
+                Solution Check: {selected === expectedAnswer ? "✔️" : "❌"}
+            </div>
         </div>
     );
 }
